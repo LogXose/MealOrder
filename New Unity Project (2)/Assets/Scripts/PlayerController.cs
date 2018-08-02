bf15d0ec77c10cfa37a3fc252d242cb16a0201b2
LogXose <37669964+LogXose@users.noreply.github.com>
2018-08-02 16:52:30 +0300

    Some Improvements

--- New Unity Project (2)/Assets/Scripts/PlayerController.cs
+++ New Unity Project (2)/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     NavMeshAgent agent;
     Animator animator;
@@ -13,65 +16,85 @@ public class PlayerController : MonoBehaviour {
     public float SPEED = 12.5f;
     public GameObject inventory;
     bool inventoryOpen = false;
-	// Use this for initialization
-	void Start () {
+
+    void Start()
+    {
         agent = GetComponent<NavMeshAgent>();
         animator = Body.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    void Update()
+    {
         Body.transform.localPosition = Vector3.zero;
         if (Input.GetMouseButtonDown(0))
         {
-            if (Time.time < InitialTouch + 0.5f)
+            if (!inventoryOpen)
             {
-
-                agent.speed = SPEED * 2;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                faster = true;
-                if (Physics.Raycast(ray, out hit))
+                if (Time.time < InitialTouch + 0.5f)
                 {
-                    agent.SetDestination(hit.point);
-                }
+                    agent.speed = SPEED * 2;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    faster = true;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        agent.SetDestination(hit.point);
+                    }
 
+                }
+                else
+                {
+                    agent.speed = SPEED;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    faster = false;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.transform.CompareTag("station"))
+                        {
+                            if (Vector3.Magnitude(hit.point - transform.position) < 10)
+                            {
+                                inventoryOpen = true;
+                                inventory.SetActive(true);
+                                inventory.transform.position = Input.mousePosition;
+                            }
+                        }
+                        agent.SetDestination(hit.point);
+                    }
+                }
+                InitialTouch = Time.time;
             }
             else
             {
-                agent.speed = SPEED;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                faster = false;
-                if (Physics.Raycast(ray, out hit))
+                GraphicRaycaster gr = inventory.GetComponent<GraphicRaycaster>();
+                PointerEventData ped = new PointerEventData(null);
+                ped.position = Input.mousePosition;
+                List<RaycastResult> results = new List<RaycastResult>();
+                gr.Raycast(ped, results);
+                if (results.Count > 0)
                 {
-                    if (hit.transform.CompareTag("station"))
+                    foreach (var item in results)
                     {
-                        if (Vector3.Magnitude(hit.point - transform.position) < 10)
-                        {
-                            inventoryOpen = true;
-                            inventory.SetActive(true);
-                            inventory.transform.position = Input.mousePosition;
-                        }
+                        //menu station show
                     }
-                    agent.SetDestination(hit.point);
+                }
+                else
+                {
+                    inventoryOpen = false;
+                    inventory.SetActive(false);
                 }
             }
-            InitialTouch = Time.time;
-
         }
 
-        if(agent.velocity.magnitude > 0.1f)
+        if (agent.velocity.magnitude > 0.1f)
         {
-            if(faster) animator.SetFloat("Speed_f", 30);
+            if (faster) animator.SetFloat("Speed_f", 30);
             else animator.SetFloat("Speed_f", 3);
         }
         else
         {
             animator.SetFloat("Speed_f", 0);
         }
-
-        
 
     }
 
